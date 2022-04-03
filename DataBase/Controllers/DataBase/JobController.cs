e61412a3e53b13800b73a6f5426296014f9e109b
Axlii50 <34549741+Axlii50@users.noreply.github.com>
2022-04-04 00:34:13 +0200

    Job Create form
    main functions for this form is done and only left thing is to add limit or just possibility to upload many files at once

--- DataBase/Controllers/DataBase/JobController.cs
+++ DataBase/Controllers/DataBase/JobController.cs
@@ -1,9 +1,12 @@
 ﻿using DataBase_Website.Data;
 using DataBase_Website.Models.DataBaseModels;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace DataBase_Website.Controllers.DataBase
@@ -11,10 +14,12 @@ namespace DataBase_Website.Controllers.DataBase
     public class JobController : Controller
     {
         private readonly DataBase_WebsiteContext _context;
+        private IHostingEnvironment Environment;
 
-        public JobController(DataBase_WebsiteContext context)
+        public JobController(DataBase_WebsiteContext context, IHostingEnvironment _env)
         {
             _context = context;
+            Environment = _env;
         }
 
         // GET: JobModels
@@ -46,17 +51,54 @@ namespace DataBase_Website.Controllers.DataBase
         }
 
         [HttpPost]
-        public IActionResult AddFile()
+        public async Task<IActionResult> AddFileAsync()
         {
             //var c = Request.Form.Files;
 
-            return PartialView("ItemPartial", "QWER");
+            foreach(IFormFile x in Request.Form.Files)
+            {
+                System.Diagnostics.Debug.WriteLine(x.FileName);
+
+                string filename = ContentDispositionHeaderValue.Parse(x.ContentDisposition).FileName.Trim('"');
+
+                filename = this.EnsureCorrectFilename(filename);
+
+                using (FileStream output = System.IO.File.Create(this.GetPathAndFilename(filename)))
+                    await x.CopyToAsync(output); 
+
+                return PartialView("ItemPartial", x.FileName);
+            }
+
+            return PartialView("ItemPartial", "test");
+        }
+
+        private string EnsureCorrectFilename(string filename)
+        {
+            if (filename.Contains("\\"))
+                filename = filename.Substring(filename.LastIndexOf("\\") + 1);
+
+            return filename;
+        }
+
+        private string GetPathAndFilename(string filename)
+        {
+            return Environment.ContentRootPath + "\\Images\\" + filename;
+             
         }
 
         [HttpGet]
         public IActionResult ReplaceItem()
         {
             return PartialView("ItemAccountsPartial",_context.AccountModel.ToListAsync().Result);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteImage(string FileName)
+        {
+
+            System.IO.File.Delete(GetPathAndFilename(FileName));
+
+            return Ok();
         }
     }
 }
